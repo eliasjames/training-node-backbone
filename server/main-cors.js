b9d13942b45780897c7be2c45157aa2d1c28fd39
Elias Carlston <elias@eliasjames.com>
2016-08-30 20:24:28 -0400

    Uncommenting CORS headers

--- server/main-cors.js
+++ server/main-cors.js
@@ -11,11 +11,11 @@ var appEnv = {
 
 app.set('view engine', 'hbs');
 
-//app.use(function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-//});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 require('./routes-cors')(app, appEnv);
 require('./errorHandling')(app, appEnv);
