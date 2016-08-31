88c68ad9a3043928005d1fef731a7e0a27242a4e
Elias Carlston <elias@eliasjames.com>
2016-08-30 21:15:06 -0400

    Backbone Hello World

--- server/public/js/clientApp.js
+++ server/public/js/clientApp.js
@@ -1,3 +1,15 @@
 var usersCollection = new Backbone.Collection;
 usersCollection.url = '/users';
 usersCollection.fetch();
+
+var userView = Backbone.View.extend({
+  el: '#app-container',
+  initialize: function(){
+    this.render();
+  },
+  render: function () {
+    this.$el.html('<h1>worked</h1>');
+  }
+});
+
+new userView();
