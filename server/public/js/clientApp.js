4c12717639d6beeae688b8dac1ece5065476afb4
Elias Carlston <elias@eliasjames.com>
2016-08-30 23:50:40 -0400

    Fetch Collection and render

--- server/public/js/clientApp.js
+++ server/public/js/clientApp.js
@@ -1,15 +1,39 @@
-var usersCollection = new Backbone.Collection;
-usersCollection.url = '/users';
-usersCollection.fetch();
+var app = {};
 
-var userView = Backbone.View.extend({
-  el: '#app-container',
-  initialize: function(){
-    this.render();
-  },
-  render: function () {
-    this.$el.html('<h1>worked</h1>');
+app.userView = Backbone.View.extend({
+  render: function() {
+    this.$el.html(
+      _.template( '<li><%= name %></li>' )( { name: this.model.get( 'name' ) })
+    );
+    return this;
   }
 });
 
-new userView();
+app.usersView = Backbone.View.extend({
+  el: '#users-container',
+  initialize: function() {
+  },
+  addOne: function( userModel ) {
+    var view = new app.userView( { model: userModel } );
+    $( '#users-list' ).append( view.render().el );
+  },
+  addAll: function() {
+    $( '#users-list' ).html('');
+    app.usersCollection.each( this.addOne, this );
+  }
+});
+app.uV = new app.usersView();
+
+app.usersCollection = Backbone.Collection.extend({});
+
+app.uC = new app.usersCollection();
+app.uC.url = '/users';
+app.uC.on( 'add', function( model ) {
+  app.uV.addOne( model );
+});
+app.uC.on( 'reset', function( model ) {
+  app.uV.addAll( model ) 
+});
+app.uC.fetch({
+  error: function() { console.log( arguments ); }
+});
