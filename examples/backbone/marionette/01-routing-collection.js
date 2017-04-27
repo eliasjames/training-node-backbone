63b73268391729585d942f2e9ebdaf6c9a5829c8
Elias Carlston <elias@eliascarlston.com>
2017-04-26 21:40:01 -0700

    Swap Marionette examples

--- examples/backbone/marionette/01-routing-collection.js
+++
@@ -1,6 +0,0 @@
-const ChatMessageCollection = Backbone.Collection.extend({
-  model: ChatMessageModel,
-  url: '/sync'
-});
-let chatMessageCollection = new ChatMessageCollection();
-chatMessageCollection.fetch();
