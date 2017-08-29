930b1d326f2150fb51461978239cf75c318ab978
Elias Carlston <elias@eliascarlston.com>
2017-08-29 17:23:23 -0400

    Typo: el versus

--- examples/backbone/collections/02-render-all-one-view.js
+++ examples/backbone/collections/02-render-all-one-view.js
@@ -5,6 +5,6 @@ const ChatMessageView = Backbone.View.extend({
   render() {
     let template = Handlebars.compile( $('#chat-message-template').html())
     let renderedTemplate = template( this.model.attributes );
-    this.el = renderedTemplate;
+    this.$el.html( renderedTemplate );
   }
 });
